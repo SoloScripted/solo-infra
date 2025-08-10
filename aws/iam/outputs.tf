99581758898c6810c17d6b524e3b89d968ae78b3
Mustafa Kara <mkara@soloscripted.com>
2025-08-10 20:54:38 +0300

    test
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>

--- aws/iam/outputs.tf
+++ aws/iam/outputs.tf
@@ -4,7 +4,7 @@ output "github_oidc_provider_arn" {
 }
 
 output "github_actions_role_arns" {
-  description = "A map of repository roles to their corresponding IAM role ARNs."
+  description = "A map of roles to their corresponding IAM role ARNs."
   value = {
     for key, role in aws_iam_role.github_actions : key => role.arn
   }
